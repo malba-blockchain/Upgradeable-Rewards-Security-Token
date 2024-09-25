57d72a4c99f8db25ec32a1383f4b381ac217ae30
Carlos Mario <cm.alba10@uniandes.edu.co>
2024-09-25 12:04:42 -0500

    created functionality to calculate token rewards from investors and team members

--- scripts/updateWeeklyRewards.ts
+++ scripts/updateWeeklyRewards.ts
@@ -11,9 +11,9 @@ const SMART_CONTRACT_NETWORK = Network.MATIC_AMOY;
 
 const AVERAGE_BLOCK_TIME_IN_BLOCKCHAIN = 2.1;
 
-const { TOKEN_SMART_CONTRACT_ADDRESS } = require('../utils/addresses.ts');
+const { TOKEN_SMART_CONTRACT_ADDRESS, REWARDS_SMART_CONTRACT_ADDRESS } = require('../utils/addresses.ts');
 
-const { TOKEN_SMART_CONTRACT_ABI } = require('../utils/abi.ts');
+const { TOKEN_SMART_CONTRACT_ABI, REWARDS_SMART_CONTRACT_ABI } = require('../utils/abi.ts');
 
 const REWARD_TOKENS_PER_YEAR = 150000000 * 10**18; // 150 Million tokens per year
 
@@ -23,8 +23,11 @@ const REWARD_TOKENS_PER_WEEK =  REWARD_TOKENS_PER_YEAR / 52; // 150 Million toke
 //Create a provider to connect to the blockchain and be able to query it
 const alchemyProvider = new ethers.JsonRpcProvider(process.env.REACT_APP_POLYGON_AMOY_RPC_URL);
 
-//Create an instance of the contract to interact with it
-const contract = new ethers.Contract(TOKEN_SMART_CONTRACT_ADDRESS, TOKEN_SMART_CONTRACT_ABI, alchemyProvider);
+//Create an instance of the token contract to interact with it
+const tokenContract = new ethers.Contract(TOKEN_SMART_CONTRACT_ADDRESS, TOKEN_SMART_CONTRACT_ABI, alchemyProvider);
+
+//Create an instance of the rewards contract to interact with it
+const rewardsContract = new ethers.Contract(REWARDS_SMART_CONTRACT_ADDRESS, REWARDS_SMART_CONTRACT_ABI, alchemyProvider);
 
 const alchemy = new Alchemy({
     apiKey: process.env.REACT_APP_ALCHEMY_API_KEY, // Replace with your Alchemy API key
@@ -36,7 +39,7 @@ const alchemy = new Alchemy({
 async function getAllTokenHolders(): Promise<Set<string>> {
 
     //Create a filter that allows you to see all events and get all transfers no matter the from nor the to
-    const transferEventFilter = contract.filters.Transfer(null, null);
+    const transferEventFilter = tokenContract.filters.Transfer(null, null);
 
     //Now add the filter the restriction of until 1 week ago
     const oneWeekAgoTimestamp = Date.now() - 7 * 24 * 60 * 60 * 1000;
@@ -44,7 +47,7 @@ async function getAllTokenHolders(): Promise<Set<string>> {
     //(transferEventFilter as any).toBlock = oneWeekAgoTimestamp; //BLOCK FROM A WEEK AGO
 
     //Get the whole list of transfer events, query filter helps determine the range of blocks to to the query
-    const transferEvents = await contract.queryFilter(transferEventFilter);
+    const transferEvents = await tokenContract.queryFilter(transferEventFilter);
 
     //Create a set to store the addresses because a set doesn't store repeated values
     const holders: Set<string> = new Set();
@@ -73,7 +76,7 @@ async function getAllWhitelistedTokenHolders(): Promise<Set<string>> {
     for (const tokenHolder of allTokenHolders) {
         //Check the address is in the whitelist
         const [isWhiteListed, isQualifiedInvestor, totalHyaxBoughtByInvestor, totalUsdDepositedByInvestor] 
-            = await contract.investorData(tokenHolder);
+            = await tokenContract.investorData(tokenHolder);
         if (isWhiteListed == true) {
             whitelistedTokenHolders.add(tokenHolder);
         }
@@ -109,7 +112,7 @@ async function getTokenBalancesAndTotalHoldings(): Promise<{ balances: Map<strin
 
     for (const whiteListedtokenHolder of whitelistedTokenHolders) {
  
-        const balance = await contract.balanceOf(whiteListedtokenHolder, {
+        const balance = await tokenContract.balanceOf(whiteListedtokenHolder, {
             blockTag: targetBlockNumber, // Fetch at a specific block
           });
 
@@ -123,40 +126,159 @@ async function getTokenBalancesAndTotalHoldings(): Promise<{ balances: Map<strin
    return { balances: whitelistedAddressesAndBalances, totalTokenHoldings: totalHoldingsLastWeek }
 }
 
-async function calculateRewardsForWallets(): Promise<{ balances: Map<string, [number, number]>, totalRewards: number }> {
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//CALCULUS FOR TEAM WALLETS
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+async function getAllTeamWallets(): Promise<Set<string>> {
+
+    // Create a filter for WalletAddedToWhitelist events with no specific parameters
+    const transferEventFilter = rewardsContract.filters.WalletAddedToWhitelist(null, null, null, null);
+
+    // Calculate the timestamp for 1 week ago to filter events up to that point
+    const oneWeekAgoTimestamp = Date.now() - 7 * 24 * 60 * 60 * 1000;
+    // Set the filter to start from the first block (0) and end at the block corresponding to 1 week ago
+    (transferEventFilter as any).fromBlock = 0;
+    //(transferEventFilter as any).toBlock = oneWeekAgoTimestamp; //BLOCK FROM A WEEK AGO
+
+    // Query the blockchain for events that match the filter
+    const addToWhitelistEvents = await rewardsContract.queryFilter(transferEventFilter);
+
+    //Create a set to store the addresses because a set doesn't store repeated values
+    const teamWallets: Set<string> = new Set();
+
+    addToWhitelistEvents.forEach((event) => {
+
+        if ("args" in event) { //Ensure the event has the expected arguments
+            const [ sender, walletAddress, isTeamWallet, hyaxHoldingAmount ] = event.args; //Destructure from and to addresses from the event
+            
+            if (isTeamWallet == true) 
+            {
+                teamWallets.add(walletAddress); //Add the from address if its not a minting transaction
+            }     
+        }
+    });
+
+    return teamWallets;
+}
+
+
+async function getAllWhitelistedTeamWallets(): Promise<Set<string>> {
+
+    var allTeamWallets = await getAllTeamWallets();
+
+    var whitelistedTeamWallets: Set<string> = new Set();
+
+    for (const teamWallet of allTeamWallets) {
+        //Check the address is in the whitelist
+        const [ hyaxHoldingAmount, hyaxHoldingAmountAtWhitelistTime, totalHyaxRewardsAmount, 
+            currentRewardsAmount, rewardsWithdrawn, addedToWhitelistTime, tokenWithdrawalTimes, 
+            lastRewardsWithdrawalTime, lastRewardsUpdateTime, isTeamWallet, isWhitelisted, isBlacklisted]
+            = await rewardsContract.wallets(teamWallet);
+
+        if (isWhitelisted == true && isBlacklisted == false ) {
+            whitelistedTeamWallets.add(teamWallet);
+        }
+    }
+
+    return whitelistedTeamWallets;
+}
+
+
+async function getTokenBalancesAndTotalHoldingsTeam(): Promise<{ balances: Map<string, number>, totalTokenHoldings: number }> {
     
-    let totalRewards = 0; 
+    const whitelistedTeamWallets = await getAllWhitelistedTeamWallets();
+
+    let totalTeamHoldingsLastWeek = 0;
+
+    let whitelistedTeamAddressesAndBalances = new Map<string, number>();
+
+    const currentBlock = await alchemy.core.getBlockNumber();
+
+    const secondsInAWeek = 7 * 24 * 60 * 60;
+
+    const blocksInAWeek = Math.floor(secondsInAWeek / AVERAGE_BLOCK_TIME_IN_BLOCKCHAIN);
+
+    //const targetBlockNumber = currentBlock - blocksInAWeek; //BLOCK IN A WEEK AGO
+
+    const targetBlockNumber = currentBlock;
+
+    console.log("[Log]: Block target number: ", targetBlockNumber);
+
+    const block = await alchemy.core.getBlock(targetBlockNumber);
+
+    const blockDate = new Date(block.timestamp * 1000); // Convert timestamp to milliseconds
+    console.log("[Log]: Block date: ", blockDate.toISOString());
+
+    for (const whiteListedTeamtokenHolder of whitelistedTeamWallets) {
+
+        const [ hyaxHoldingAmount, hyaxHoldingAmountAtWhitelistTime, totalHyaxRewardsAmount, 
+            currentRewardsAmount, rewardsWithdrawn, addedToWhitelistTime, tokenWithdrawalTimes, 
+            lastRewardsWithdrawalTime, lastRewardsUpdateTime, isTeamWallet, isWhitelisted, isBlacklisted]
+            = await rewardsContract.wallets(whiteListedTeamtokenHolder,{
+                blockTag: targetBlockNumber, // Fetch at a specific block
+            });
+
+        const formattedBalance = Number(ethers.formatEther(hyaxHoldingAmountAtWhitelistTime.toString()));
+
+        whitelistedTeamAddressesAndBalances.set(whiteListedTeamtokenHolder,formattedBalance);
+
+        totalTeamHoldingsLastWeek += formattedBalance;
+    }
+
+   return { balances: whitelistedTeamAddressesAndBalances, totalTokenHoldings: totalTeamHoldingsLastWeek }
+}
+
+async function calculateRewardsForTeamWallets(): Promise<{ balances: Map<string, [bigint, number]>, totalRewards: bigint }> {
+    
+    let totalRewards = BigInt(0); 
 
     let totalPercentage = 0.0;
 
     //Mapping stores rewards per wallet and percentage of those rewards
-    let rewardsForWallets = new Map<string, [number, number]>();
+    let rewardsForWallets = new Map<string, [bigint, number]>();
 
-    const tokenBalances = (await getTokenBalancesAndTotalHoldings()).balances;
+    const tokenHoldingsInvestors = (await getTokenBalancesAndTotalHoldings()).totalTokenHoldings;
 
-    const totalTokenHoldings = (await getTokenBalancesAndTotalHoldings()).totalTokenHoldings;
+    const tokenHoldingsTeam = (await getTokenBalancesAndTotalHoldingsTeam()).totalTokenHoldings;
 
-    for (const balanceTokenHolder of tokenBalances) {
+
+    const tokenBalancesInvestors = (await getTokenBalancesAndTotalHoldings()).balances;
+    
+    const tokenBalancesTeam = (await getTokenBalancesAndTotalHoldingsTeam()).balances;
+
+
+    const totalTokenBalances = new Set([...tokenBalancesInvestors, ...tokenBalancesTeam]);
+
+    const totalTokenHoldings = tokenHoldingsInvestors + tokenHoldingsTeam;
+
+
+    for (const balanceTokenHolder of totalTokenBalances) {
+
         const percentageRewards = balanceTokenHolder[1] / totalTokenHoldings;
 
-        const amountRewards = percentageRewards * REWARD_TOKENS_PER_WEEK;
-
-        console.log("Address: ", balanceTokenHolder[0], ". Rewards: ",amountRewards ,". Percentage: ", percentageRewards*100, "%");
-
+        // Convert the product of percentageRewards and REWARD_TOKENS_PER_WEEK to a string to avoid overflow
+        const amountRewards = BigInt(percentageRewards * REWARD_TOKENS_PER_WEEK);
+        
+        console.log("\nAddress: ", balanceTokenHolder[0], ". Token rewards: ", Number(ethers.formatEther(amountRewards.toString())) ,
+            ". Percentage: ", percentageRewards*100, "%");
+        
         totalRewards += amountRewards;
 
         totalPercentage += percentageRewards;
+
+        rewardsForWallets.set(balanceTokenHolder[0], [amountRewards, percentageRewards]);
     }
 
     console.log("\nPercentage rewards: ", totalPercentage*100, "%");
     console.log("Expected rewards:", REWARD_TOKENS_PER_WEEK);
-    console.log("Distributed rewards: ", totalRewards);
+    console.log("Distributed rewards: ",  Number(ethers.formatEther(totalRewards.toString())) );
 
     return { balances: rewardsForWallets, totalRewards: totalRewards }
-
 }
 
 async function main() {
+    /*
     console.log("\nList of all token holders");
     const allTokenHolders = await getAllTokenHolders();
     console.log(allTokenHolders);
@@ -165,15 +287,34 @@ async function main() {
     const whitelistedTokenHolders = await getAllWhitelistedTokenHolders();
     console.log(whitelistedTokenHolders);
     
-    console.log("\nList of whitelisted token holders and their balances");  
-    const tokenBalances = (await getTokenBalancesAndTotalHoldings()).balances;
-    console.log(tokenBalances);
 
     console.log("\nTotal holdings last week");
     const totalTokenHoldings = (await getTokenBalancesAndTotalHoldings()).totalTokenHoldings;
     console.log(totalTokenHoldings);
 
-    await calculateRewardsForWallets();
+    console.log("\nList of all team wallets");
+    const allTeamWallets = await getAllTeamWallets();
+    console.log(allTeamWallets);
+
+    console.log("\nList of all whitelisted team wallets");  
+    const whitelistedTeamWallets = await getAllWhitelistedTeamWallets();
+    console.log(whitelistedTeamWallets);
+
+    console.log("\nTotal team holdings last week");
+    const totalTeamTokenHoldings = (await getTokenBalancesAndTotalHoldingsTeam()).totalTokenHoldings;
+    console.log(totalTeamTokenHoldings);
+    */
+
+    console.log("\nList of whitelisted token holders and their balances");  
+    const tokenBalances = (await getTokenBalancesAndTotalHoldings()).balances;
+    console.log(tokenBalances);
+
+    console.log("\nList of whitelisted team token holders and their balances");  
+    const tokenTeamBalances = (await getTokenBalancesAndTotalHoldingsTeam()).balances;
+    console.log(tokenTeamBalances);
+
+    await calculateRewardsForTeamWallets();
+
 }
 
 main(); // Call the async function
